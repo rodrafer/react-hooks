5105952fab9161cddbc905bc71d05df346d8781d
Rodrigo Fernandez Frittelli <rodrigo.frittelli@mercadolibre.com>
2024-05-09 16:58:59 -0300

    exercise-01 with extra credit

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,19 +3,31 @@
 
 import * as React from 'react'
 
-function Greeting() {
+function Greeting({ initialName = '' }) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  // const name = ''
+
+  /**
+   * You can reset a component’s state by passing a different key to a component. In this example,
+   * the Reset button changes the version state variable, which we pass as a key to the Form.
+   * When the key changes, React re-creates the Form component (and all of its children) from scratch,
+   * so its state gets reset.
+   * https://react.dev/reference/react/useState#resetting-state-with-a-key
+   */
+  
+  // See https://react.dev/reference/react/useState for more on useState
+  const [name, setName] = React.useState(initialName);
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
+    setName(event.target.value);
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -23,7 +35,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Kent" />
 }
 
 export default App
