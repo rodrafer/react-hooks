b0dba1ab353fec9cc1df10b091b149723b5ccc7a
Rodrigo Fernandez Frittelli <rodrigo.frittelli@mercadolibre.com>
2024-05-16 00:41:41 -0300

    exercise-02 with extra credits

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,18 +3,67 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
+// - Extra Credit 03 -
+// const useLocalStorageState = (key) => {
+//   React.useEffect(() => {
+//     // window.localStorage.setItem('name', key);
+//     // - Extra Credit 04 -
+//     window.localStorage.setItem('name', JSON.parse(JSON.stringify(key)));
+//        - Extra Credit 04 Final -
+//        See 02.extra-4.js
+//   }, [key]);
+// }
+
+// - Extra Credit 03 Final -
+const useLocalStorageState = (key, defaultValue = '') => {
+  const [state, setState] = React.useState(() => window.localStorage.getItem(key) ?? defaultValue);
+
+  React.useEffect(() => {
+    window.localStorage.setItem(key, state);
+  }, [key, state]);
+
+  return [state, setState];
+}
+
+function Greeting({initialName = 'Joe'}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+
+  // - Base Solution -
+  // const initialNameValue = window.localStorage.getItem('name') ?? initialName;
+  // const [name, setName] = React.useState(initialNameValue);
+
+  // - Extra Credit 01 -
+  // const retrieveInitialNameValue = () => window.localStorage.getItem('name') ?? initialName;
+  // const [name, setName] = React.useState(retrieveInitialNameValue);
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
 
+  // - Base Solution -
+  // This effect is called on _every_ rerender. This is not ideal because
+  // we only wantto update localStorage when the name changes.
+  // React.useEffect(() => {
+  //   window.localStorage.setItem('name', name);
+  // })
+
+  // - Extra Credit 02 -
+  // This effect is called on every rerender that the name changes.
+  // React.useEffect(() => {
+  //   window.localStorage.setItem('name', name);
+  // }, [name]);
+
+  // - Extra Credit 03 -
+  // useLocalStorageState(name);
+
+  // - Extra Credit 03 Final -
+  const [name, setName] = useLocalStorageState('name', initialName);
+
   function handleChange(event) {
     setName(event.target.value)
   }
+
   return (
     <div>
       <form>
